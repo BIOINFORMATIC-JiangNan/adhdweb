e95046409e08bb597077309a42b6322c2fc40067
YueYing Liu Lab <112561727+BIOINFORMATIC-JiangNan@users.noreply.github.com>
2024-06-27 18:43:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import catboost as xgb
+import xgbboost as xgb
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
